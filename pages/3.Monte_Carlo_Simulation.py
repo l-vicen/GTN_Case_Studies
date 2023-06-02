c7503772ecbccd9c062db309d9ca1d0cf5b2292f
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-06-02 14:21:15 +0200

    DEV: test

--- pages/3.Monte_Carlo_Simulation.py
+++ pages/3.Monte_Carlo_Simulation.py
@@ -34,3 +34,18 @@ st.markdown("### Fixation Probabilities")
 st.write(fixation_probabilities)
 
 stationary_distribution = egt.utils.calculate_stationary_distribution(transition_matrix.transpose())
+st.markdown("### Stationary Distribution")
+st.write(stationary_distribution)
+
+fig, ax = plt.subplots(figsize=(5, 5), dpi=150)
+G = egt.plotting.draw_invasion_diagram(strategy_labels,
+                                              1/Z, fixation_probabilities, stationary_distribution,
+                                              node_size=600, 
+                                              font_size_node_labels=8,
+                                              font_size_edge_labels=8,
+                                              font_size_sd_labels=8,
+                                              edge_width=1,
+                                              min_strategy_frequency=0.00001, 
+                                              ax=ax)
+plt.axis('off')
+st.pyplot(fig)
