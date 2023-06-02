ff1f672954953bc2d43fa3cf3f78cc4f0def5014
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-06-02 16:13:41 +0200

    DEV: test

--- pages/3Monte_Carlo_Simulation.py
+++ pages/3Monte_Carlo_Simulation.py
@@ -1,37 +1,51 @@
-import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import egttools as egt
-import networkx as nx
-
-from egttools.behaviors.NormalForm.TwoActions import (Cooperator,Defector, TFT, Pavlov, GRIM, Random)
-
+import streamlit as st
+from egttools.behaviors.NormalForm.TwoActions import (Cooperator, Defector, Random)
 
 egt.Random.init()
 seed = egt.Random._seed
 
-Z = 100
-beta = 1.0
+A = np.array([
+    [1, 0, 0],
+    [-1, -1, -1],
+    [-0.5,-0.5,-0.5]
+])
 
-# Define which strategies can be present in the population
-strategies = [Cooperator(), Defector(), TFT(), Pavlov(), Random(), GRIM()]
-# define the game
-payoffs = np.array([[1, 3],[0, 2]])
-game = egt.games.NormalFormGame(100, payoffs, strategies)
-# Define the parameters of the population and instantiate the evolver
+strategies = [egt.behaviors.NormalForm.TwoActions.Cooperator(),
+              egt.behaviors.NormalForm.TwoActions.Defector(),
+              egt.behaviors.NormalForm.TwoActions.Random()]
+
+strategy_labels = [strategy.type().replace("NFGStrategies::", '') for strategy in strategies]
+
+game = egt.games.NormalFormGame(100, A, strategies)
+
+Z= 100; beta=1
 evolver = egt.analytical.PairwiseComparison(Z, game)
-# Calculate the transition and fixation probabilities and the stationary distribution
+st.write(evolver)
+
 transition_matrix,fixation_probabilities = evolver.calculate_transition_and_fixation_matrix_sml(beta)
-stationary_distribution = egt.utils.calculate_stationary_distribution(transition_matrix)
-# Plot the invasion diagram
-G = egt.plotting.draw_invasion_diagram([strategy.type().replace("NFGStrategies:", "") for strategy in
-strategies], 1/Z, fixation_probabilities, stationary_distribution)
 
-fig, ax = plt.subplots()
-pos = nx.kamada_kawai_layout(G)
-nx.draw(G,pos, with_labels=True)
-st.pyplot(fig)
+st.markdown("### Transformation Matrix")
+st.write(transition_matrix)
 
+st.markdown("### Fixation Probabilities")
 st.write(fixation_probabilities)
 
+stationary_distribution = egt.utils.calculate_stationary_distribution(transition_matrix.transpose())
+st.markdown("### Stationary Distribution")
 st.write(stationary_distribution)
+
+fig, ax = plt.subplots(figsize=(5, 5), dpi=150)
+G = egt.plotting.draw_invasion_diagram(strategy_labels,
+                                              1/Z, fixation_probabilities, stationary_distribution,
+                                              node_size=600,
+                                              font_size_node_labels=8,
+                                              font_size_edge_labels=8,
+                                              font_size_sd_labels=8,
+                                              edge_width=1,
+                                              min_strategy_frequency=0.00001,
+                                              ax=ax)
+plt.axis('off')
+st.pyplot(fig)
