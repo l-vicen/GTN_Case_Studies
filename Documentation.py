267c662bbfeef6c59d593befd77f0043d6e4647a
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-06-04 00:42:04 +0200

    DEV: Macros

--- Documentation.py
+++ Documentation.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
 st.title("GTN Case Study: Evolutionary Game Theory")
+
+st.warning("Under Construction...")
+
+st.write("Evolutionary methods are implemented through EGTTools Library")
+st.info("@misc{Fernandez2020,\
+  author = {Fernández Domingos, Elias},\
+  title = {EGTTools: Toolbox for Evolutionary Game Theory},\
+  year = {2020},\
+  publisher = {GitHub},\
+  journal = {GitHub repository},\
+  howpublished = {\url{https://github.com/Socrats/EGTTools}},\
+  doi = {10.5281/zenodo.3687125}}")
