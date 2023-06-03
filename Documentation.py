5b310125f16433393b2dc3505179cbd998f6acb0
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-06-04 00:44:00 +0200

    DEV: Macros

--- Documentation.py
+++ Documentation.py
@@ -5,7 +5,7 @@ st.title("GTN Case Study: Evolutionary Game Theory")
 st.warning("Under Construction...")
 
 st.write("Evolutionary methods are implemented through EGTTools Library")
-st.info("@misc{Fernandez2020,\
+st.write("@misc{Fernandez2020,\
   author = {Fernández Domingos, Elias},\
   title = {EGTTools: Toolbox for Evolutionary Game Theory},\
   year = {2020},\
