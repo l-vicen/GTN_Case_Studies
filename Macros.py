173c15320fc1977d23e2d97701d7af4f0345b453
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-06-05 07:47:13 +0200

    DEV: macros

--- Macros.py
+++ Macros.py
@@ -21,27 +21,27 @@ BASIC_MODEL_PAYOFF = np.array([
 ])
 
 FINES_MODEL_PAYOFF = np.array([
-        [-1, -5.7, -3.35], # Mask 
-        [ -14.7, -98.36, -54.18], # No Mask
-        [-12.35, -54.18, -32.09] # Dynamic Mask
+        [-1.25, -5.7, -3.35], # Mask 
+        [ 15.3, -68.36, -24.18], # No Mask
+        [2.65, -39.18, -17.09] # Dynamic Mask
 ])
 
 SUBSIDIES_MODEL_PAYOFF = np.array([
-        [4, -0.7, -1.65], # Mask 
-        [ -14.7, -98.36, -54.18], # No Mask
-        [-12.35, -54.18, -32.09] # Dynamic Mask
+        [3.75, -0.7, 1.65], # Mask 
+        [25.3, -58.36, -14.18], # No Mask
+        [12.65, -29.18, -7.09] # Dynamic Mask
 ])
 
 LOW_MED_MODEL_PAYOFF = np.array([
-        [-1.38, -8.05, -4.53], # Mask 
-        [ -7.05, -132.54, -66.27], # No Mask
-        [-3.53, -66.27, -33.14] # Dynamic Mask
+        [-1.375, -8.05, -4.525], # Mask 
+        [ 22.95, -102.54, -36.27], # No Mask
+        [-11.475, -51.27, -18.135] # Dynamic Mask
 ])
 
 HIGH_MED_MODEL_PAYOFF = np.array([
-        [-1, -4.76, -2.88], # Mask 
-        [ -3.76, -70.69, -35.34], # No Mask
-        [-1.88, -35.34, -17.67] # Dynamic Mask
+        [-1.2, -4.76, -2.88], # Mask 
+        [ 26.24, -40.688, -5.3444], # No Mask
+        [13.12, -20.344, -2.672] # Dynamic Mask
 ])
 
 LOCAL_MODEL_PAYOFF_DICT = {
@@ -50,5 +50,5 @@ LOCAL_MODEL_PAYOFF_DICT = {
     LOCAL_MODELS[3]: SUBSIDIES_MODEL_PAYOFF,
     LOCAL_MODELS[4]: LOW_MED_MODEL_PAYOFF,
     LOCAL_MODELS[5]: HIGH_MED_MODEL_PAYOFF,
-    LOCAL_MODELS[6]: LUCAS_THREE
+#     LOCAL_MODELS[6]: LUCAS_THREE
 }
