5d020ac2f9d77d9f6023062638d5cb53e737ee95
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-07-09 18:07:20 +0200

    Docu 3

--- pages/3Agent_Based_Simulation.py
+++ pages/3Agent_Based_Simulation.py
@@ -1,3 +1,4 @@
+# Dependencies both indoor and from third party
 import numpy as np
 import matplotlib.pyplot as plt
 import egttools as egt
@@ -9,16 +10,18 @@ from cycler import cycler
 import Macros
 from egttools.behaviors.NormalForm.TwoActions import (Cooperator, Defector, Random)
 
+# Default settings of plots
 sns.set_style("whitegrid")
 plt.rcParams['svg.fonttype'] = 'none'
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
 egt.Random.init()
 seed = egt.Random._seed
 
+# Heading ABS
 st.markdown("# Agent-Based-Simulation")
 st.markdown("## Inputs")
 
+# User inputs
 population = int(st.number_input('Insert population size', value=1200, help="Make sure it is a number divisible by 3 since we have three agents."))
 allC = int(st.number_input('Insert number of Cooperator agents in the population', value=400))
 allD = int(st.number_input('Insert number of Defector agents in the population', value=400))
@@ -28,25 +31,45 @@ beta = st.number_input('Insert the Selection Strength', value = 1.0, format="%.2
 selected_payoff = st.selectbox("Select the desired payoff matrix representing a local model.", Macros.LOCAL_MODELS)
 rounds = 1
 
+# Dont allow user to proceed without having selected a model
 if (selected_payoff != "None"): 
-
+   
+   # Making sure all params are given
    if st.button('Simulate'):
+        
+        # Query the payoff matrix associated with user selection
         A = Macros.LOCAL_MODEL_PAYOFF_DICT[selected_payoff]
+
+        # Calling out pre-defined behavior 
         strategies = [Cooperator(), Defector(), Random()]
-        game = egt.games.NormalFormGame(rounds, A, strategies)
         strategy_labels = [strategy.type().replace("NFGStrategies::", '') for strategy in strategies]
+
+        # Create NormalFormGame Representation
+        game = egt.games.NormalFormGame(rounds, A, strategies)
+
+        # Model payoff    
         st.markdown("## Payoff Matrix")
         st.write(A)
 
+        # Start of output section
         st.markdown("---")
         st.markdown("## Output")
 
+        # 1 Run of numerical simulation as shown in EGTtool tutorial
         st.markdown("### Population Development over Generation")
         st.info("Here 1 single run has been considered.")
+
+        # Creates the evolver object 
         evolver = egt.numerical.PairwiseComparisonNumerical(population, game, 1000)
+       
+        # Run simulation once for 1e7 generations and starting agent population states
         output = evolver.run(int(1e7), 1, 1e-3, [allC, allD, allRandom])
+        
+        # Setting up plot configurations (style)
         colors = sns.color_palette("colorblind", len(strategies))
         plt.rc('axes', prop_cycle=(cycler('color', colors)))
+
+        # Creates matplotlib figure
         fig, ax = plt.subplots(figsize=(10, 4))
         lines = ax.plot(np.arange(1e7+1)[::100], output[::100]/population)
         plt.setp(lines, linewidth=2)
@@ -63,16 +86,25 @@ if (selected_payoff != "None"):
             tick.label1.set_fontweight('bold')
         for tick in ax.yaxis.get_major_ticks():
             tick.label1.set_fontweight('bold')
+
+        # Adds figure to app
         st.pyplot(fig)
 
+        # Now consider 10 runs and take the average of the observed population frequencies as in EGTtool Tutorial
         st.markdown("### Strategy Distribution")
         st.info("Here 10 runs have been considered and the frequency is the average of them.")
+        
+        # Calculates the distribution
         distribution = evolver.estimate_strategy_distribution(10, int(1e7), int(1e3), beta, mu)
+        
+        # Creates 2nd Figure
         figTwo, ax = plt.subplots(figsize=(8, 3))
         ax = sns.barplot(x=strategy_labels, y=distribution)
         ax.set_ylabel('frequency', fontsize=15)
         ax.tick_params(bottom = False, labelsize=15)
         sns.despine()
+
+        # Adds to app
         st.pyplot(figTwo)
    else:
         st.warning('Once the inputs are set, press "Simulate"')
